333bedf3c137ab95fd31c12e894fbf4139d1ebf3
Adrian Mihu <adrian.mihu20@icloud.com>
2025-11-26 16:09:08 +0200

    Fabric project initialization - Client

---
+++ src/main/java/ro/maleficent/rose_gold_alloy/RoseGoldAlloyClient.java
@@ -0,0 +1,11 @@
+package ro.maleficent.rose_gold_alloy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoseGoldAlloyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ro/maleficent/rose_gold_alloy/RoseGoldAlloyDataGenerator.java
@@ -0,0 +1,11 @@
+package ro.maleficent.rose_gold_alloy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RoseGoldAlloyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
